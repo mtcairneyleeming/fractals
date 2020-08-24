dcd8a4754d189e361c5616d3297f8c5f4164509b
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2020-08-24 21:20:44 +0100

    Add first version of layer diagram

--- web/web/ui/diagrams.ts
+++ web/web/ui/diagrams.ts
@@ -2,6 +2,7 @@ import rough from "roughjs/bin/rough"
 import { RoughCanvas } from "roughjs/bin/canvas";
 let Offset = require("polygon-offset")
 import { off } from "@svgdotjs/svg.js";
+import { parseSettings } from "./settings";
 function getInput(id: string): HTMLInputElement {
     return document.getElementById(id) as unknown as HTMLInputElement
 }
@@ -14,6 +15,9 @@ let curveCanvas = null
 
 let parallel: RoughCanvas = null
 let parallelCanvas = null
+
+let layers: RoughCanvas = null
+let layersCanvas = null
 export function setupDiagrams() {
     everywhereCanvas = document.getElementById("everywhere-diagram") as HTMLCanvasElement;
     everywhere = rough.canvas(everywhereCanvas);
@@ -27,15 +31,24 @@ export function setupDiagrams() {
     parallelCanvas.width = parallelCanvas.offsetWidth;
     parallel = rough.canvas(parallelCanvas, { options: { roughness: 0.5 } })
 
+    layersCanvas = document.getElementById("layer-diagram") as HTMLCanvasElement
+    layersCanvas.style.width = "100%"
+    layersCanvas.width = layersCanvas.offsetWidth;
+    layers = rough.canvas(layersCanvas, { options: { roughness: 0.5 } })
+
 
     getInput("ev_hole_number").addEventListener("change", updateEverywhere)
     getInput("ev_ratio").addEventListener("change", updateEverywhere)
     getInput("ev_frame_percent").addEventListener("change", updateEverywhere)
     getInput("curve_frac").addEventListener("change", drawCurve)
     getInput("frame_factor").addEventListener("change", drawParallel)
+    getInput("num_layers").addEventListener("change", drawLayers)
+    getInput("draw_axiom_check").addEventListener("change", drawLayers)
+    getInput("layer_dist").addEventListener("change", drawLayers)
     drawCurve()
     updateEverywhere()
     drawParallel()
+    drawLayers()
 }
 
 function length(a) {
@@ -171,6 +184,52 @@ function drawCurve() {
     }
 }
 
+function drawLayers() {
+    let context = layersCanvas.getContext("2d")
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    context.clearRect(0, 0, layersCanvas.width, layersCanvas.height);
+    //context.translate(30, layersCanvas.height)
+    //context.scale(1, -1)
+    let settings = parseSettings()
+    let num_layers = Math.max(settings["num_layers"], 1)
+    let draw_axiom = getInput("draw_axiom_check").checked;
+    let dist_between = Math.max(0, Math.min(1.0, 0.01 * parseFloat(getInput("layer_dist").value)))
+    let scale_factor = settings["scale_factor"]
+    let tot_height = 0
+    let step = 1;
+    for (let i = 0; i < num_layers; i++) {
+        if (i != 0 || draw_axiom) {
+            tot_height += dist_between * step;
+        }
+        step *= scale_factor
+
+    }
+    let offsetY = 0.1 * layersCanvas.height;
+    let currPos = 0;
+    let yScale = 0.88 * layersCanvas.height / tot_height
+    step = 1
+    context.font = `1rem sans-serif`
+    context.textAlign = "left"
+    context.textBaseline = "top"
+    let oldHeight = Number.POSITIVE_INFINITY
+    for (let i = 0; i <= num_layers; i++) {
+        if (i != 0 || draw_axiom) {
+            let currHeight = layersCanvas.height - (yScale * currPos + offsetY)
+            layers.line(0.05 * layersCanvas.width, currHeight, (0.05 + step * 0.45) * layersCanvas.width, currHeight)
+
+             if (i == 0 && draw_axiom) {
+                context.fillText("Axiom (Layer #0)", 0.55 * layersCanvas.width, currHeight)
+            }
+            else {
+                context.fillText(`Layer #${i}`, 0.55 * layersCanvas.width, currHeight)
+                layers.line((0.05 + step * 0.45) * layersCanvas.width, currHeight, (0.05 + step * 0.45) * layersCanvas.width, oldHeight, { stroke: "blue" })
+            }
+            oldHeight = currHeight
+            currPos += dist_between * step
+        }
+        step *= scale_factor
+    }
+}
 
 function drawParallel() {
     let context = parallelCanvas.getContext("2d")
