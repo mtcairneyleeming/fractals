83264ffbc72caf1b27d7b6c896cba3e7a1cdb929
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2020-09-10 22:42:43 +0100

    Improve update script

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 echo "Updating fractals project"
-git fetch
+git pull
 
 cd ./server
 cargo build --release
 cd ../web
 parcel build web/*.html
 cd ../
+systemctl restart caddy.service
+systemctl restart rust-fractals.service 
 echo "Done."
