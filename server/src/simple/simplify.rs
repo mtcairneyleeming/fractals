dc81eb5b664db19a7fc24831b33b2bac2dce559e
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2020-09-29 14:49:14 +0100

    Remove debugging code that I committed by accident

--- server/src/simple/simplify.rs
+++ server/src/simple/simplify.rs
@@ -5,10 +5,8 @@ pub fn simplify(layers: Vec<Layer<Line3d>>) -> Vec<Layer<Line3d>> {
     for old_layer in layers {
         let mut layer = vec![old_layer.lines[0]];
         for line in old_layer.lines.iter().skip(1) {
-            old_len += line.length();
             let prev = layer.last().unwrap();
             if prev.is_parallel_to(*line) && prev.direction().add(line.direction()).norm() > 1e-7 {
-                i += 1;
                 let new_line = Line3d::new(prev.start(), line.end());
                 layer.push(new_line)
             } else {
